5fd5175d06f07b810cd3f68cb36af0f320fe3292
j0k3em <dmitry.lyampasov@yandex.ru>
2022-10-02 18:56:21 +0500

    выполнил 4 домашнюю работу

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
-const getRandom = (min, max) => {
+const NAMES = [
+  'Mike Wazowski',
+  'James Sullivan',
+  'Buzz Lightyear'
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const DESCRIPTIONS = [
+  'Увлекаюсь фотографией!',
+  'Случайный снимок',
+  'Снимок с новой камеры!'
+];
+
+const COUNT_PUBLICATIONS = 25;
+
+const arrayPublications = [];
+
+const pushArray = (count) => {
+  const array = [];
+  for (let i = 0; i <= count; i++) {
+    array.push(i);
+  }
+  return array;
+};
+
+const randomNumber = (min, max) => {
   if (min > max) {
     [min, max] = [max, min];
   }
@@ -8,9 +41,34 @@ const getRandom = (min, max) => {
   return Math.round(Math.random() * (max - min) + min);
 };
 
-const checkMaxSymbols = (string, maxSymbols) => {
-  if (string.lenght <= maxSymbols) {
-    return true;
-  }
-  return false;
+const pushArraySplice = (array) => {
+  const number = array[randomNumber(0, array.lenght - 1)];
+  array.splice(array.indexOf(number, 1));
+  return number;
 };
+
+const arrayURL = pushArray(25);
+const arrayID = pushArray(25);
+const arrayIDComments = pushArray(100);
+const commentsArray = (count) => {
+  const arrayComments = [];
+  for (let i = 0; i < count; i++) {
+    arrayComments[i] = {
+      id: pushArraySplice(arrayIDComments),
+      avatar: `img/avatar-${randomNumber(1, 6)}.svg`,
+      message: MESSAGES[randomNumber(0, MESSAGES.length - 1)],
+      name: NAMES[randomNumber(0, NAMES - 1)]
+    };
+  }
+  return commentsArray;
+};
+
+for (let i = 0; i < COUNT_PUBLICATIONS; i++) {
+  arrayPublications[i] = {
+    id: pushArraySplice(arrayID),
+    URL: `photos/${pushArraySplice(arrayURL)}.jpg`,
+    description: DESCRIPTIONS[randomNumber(0, DESCRIPTIONS - 1)],
+    likes: randomNumber(15, 100),
+    comments: commentsArray(2),
+  };
+}
